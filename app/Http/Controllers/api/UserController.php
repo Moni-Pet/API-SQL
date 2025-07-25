bf328347aa1a0e7c818f7a5fa833008cce1bb485
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-07-25 17:11:57 -0600

    fix(api): PetController & UserController
    Se agrego ruta para ver las mascotas que le pertenecen a un usuario
    
    Se modificó la función me para obtener la información del usuario
    
    Se agrega la ruta para las mascotas de usuarios

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -9,11 +9,28 @@ class UserController extends Controller
 {
     public function me(Request $request) {
         $user = $request->user();
+
         if (! $user) {
             return response()->json([
-                'status' => false,
-                'msg' => ''
-            ]);
+                'result' => false,
+                'msg' => 'No autenticado',
+                'error_code' => 401,
+                'data' => null,
+            ], 401);
         }
+
+        return response()->json([
+            'result' => true,
+            'msg' => 'Usuario autenticado',
+            'error_code' => null,
+                'data' => [
+                    'name'       => $user->name,
+                    'last_name'  => $user->last_name,
+                    'last_name2' => $user->last_name2,
+                    'email'      => $user->email,
+                    'gender'     => $user->gender,
+                    'birth_date' => $user->birth_date,
+                ],
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\api\Pet\PetController;
 use App\Http\Controllers\api\Pet\PetTypeController;
 use App\Http\Controllers\api\Service\ServiceTypeController;
 use App\Models\ServiceType;
+use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Routing\RouteRegistrar;
 use Illuminate\Support\Facades\Auth;
@@ -25,11 +26,24 @@ use Laravel\Sanctum\Sanctum;
 */
 
 // Rutas sin protección
+//Auth
 Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::get('/email/verify', [EmailController::class, 'activateAccount'])->name('activate.account');
+    Route::post('/email/resend-verification', [AuthController::class, 'resendEmailVerification']);
 });
+
+// TypesPet
+route::get('/types_pet', [PetTypeController::class, 'index']);
+route::get('/types_pet/{id}', [PetTypeController::class, 'show']);
+
+// Breeds
+route::get('/breeds', [BreedController::class, 'index']);
+route::get('/breeds/{id}', [BreedController::class, 'show']);
+
+// Pets
+route::get('/pets', [PetController::class, 'index']);
+route::get('/pets/{id}', [PetController::class, 'show']);
 
 //Services
 Route::get('/type_service', [ServiceTypeController::class, 'index']);
@@ -43,6 +57,7 @@ Route::group(
     ],
     function () {
         // Rutas sin token
+        Route::post('/auth/login', [AuthController::class, 'login']);
         Route::post('/auth/2af/verify', [AuthController::class, 'verifyTAF'])->middleware('verifiedaccount');
         Route::post('/auth/2af/send', [AuthController::class, 'resendVerificationCode']);
 
@@ -51,30 +66,34 @@ Route::group(
         Route::group([
             'middleware' => ['auth:sanctum']
         ], function () {
-            
+
             // Rutas admin
             Route::group([
                 'middleware' => ['usertype:1']
             ], function () {
                 //
+
             });
 
             // Rutas Empleado y Admin
-            Route::group(['middleware' => ['usertype:1,2']], function () {
-                route::get('/types_pet', [PetTypeController::class, 'index']);
-                route::get('/types_pet/{id}', [PetTypeController::class, 'show']);
+            Route::group([
+                'middleware' => ['usertype:1,2']
+            ], function () {
+                //
+                Route::get('/user', [UserController::class, 'index']);
+                Route::get('/user/{id}', [UserController::class, 'show']);
+                Route::post('/user', [UserController::class, 'store']);
+                Route::put('/user/{id}', [UserController::class, 'update']);
+                Route::delete('/user', [UserController::class, 'destroy']);
+
                 Route::post('/types_pet', [PetTypeController::class, 'store']);
                 Route::put('/types_pet/{id}', [PetTypeController::class, 'update']);
                 Route::delete('/types_pet/{id}', [PetTypeController::class, 'destroy']);
 
-                route::get('/breeds', [BreedController::class, 'index']);
-                route::get('/breeds/{id}', [BreedController::class, 'show']);
                 Route::post('/breeds', [BreedController::class, 'store']);
                 Route::put('/breeds/{id}', [BreedController::class, 'update']);
                 Route::delete('/breeds/{id}', [BreedController::class, 'destroy']);
 
-                route::get('/pets', [PetController::class, 'index']);
-                route::get('/pets/{id}', [PetController::class, 'show']);
                 Route::post('/pets', [PetController::class, 'store']);
                 Route::put('/pets/{id}', [PetController::class, 'update']);
                 Route::delete('/pets/{id}', [PetController::class, 'destroy']);
@@ -95,6 +114,12 @@ Route::group(
             });
             // Rutas compartidas
             Route::post('/auth/logout', [AuthController::class, 'logout']);
+            Route::get('/auth/me', [UserController::class, 'me']);
+
+            //Rutas que comparten todo los tipo de usuarios
+            Route::group(['middleware' => ['usertype:1,2,3,4']], function () {
+                Route::get('/pets/adopter/{id}', [PetController::class, 'showUserPets']);
+            });
         });
     }
 );
