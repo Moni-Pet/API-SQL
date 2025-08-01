29adc77384973f2e8ba61ab4f3fccbd445ebb044
Jess JH <89726090+Icelii@users.noreply.github.com>
2025-08-01 06:18:00 -0600

    fix(api): Middleware UserType
    *No funcionaba con múltiples user types.

--- app/Http/Middleware/UserTypeMiddleware.php
+++ app/Http/Middleware/UserTypeMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserTypeMiddleware
@@ -13,11 +14,16 @@ class UserTypeMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $user_type): Response
+    public function handle(Request $request, Closure $next, ...$user_type): Response
     {
         $user = $request->user();
-        $allowedTypes = explode(',', $user_type);
-        if(!$user || !in_array($user->user_type_id, $allowedTypes)){
+        $allowedTypes = array_map('intval', $user_type);
+        $userTypeId = (int) $user->user_type_id;
+
+        //Log::info('User type id:', ['type' => gettype($user->user_type_id), 'value' => $user->user_type_id]);
+        //Log::info('Allowed types:', $allowedTypes);
+
+        if (!$user || !in_array($userTypeId, $allowedTypes)) {
             return response()->json([
                 'result' => false,
                 'msg' => 'No tienes permiso para realizar esta acción',
@@ -25,6 +31,7 @@ class UserTypeMiddleware
                 'data' => null
             ], 403);
         }
+
         return $next($request);
     }
 }
