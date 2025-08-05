e7950d6ec90c653af2a3b100f7b46782ca98a4e8
fernandom11399 <jafetavalos49@gmail.com>
2025-08-04 20:11:09 -0600

    Update 2025_08_04_193644_create_lost_pets_table.php

--- database/migrations/2025_08_04_193644_create_lost_pets_table.php
+++ database/migrations/2025_08_04_193644_create_lost_pets_table.php
@@ -13,12 +13,12 @@ return new class extends Migration
     {
         Schema::create('lost_pets', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('pet_id')->constrained('pets');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('pet_id')->references('id')->on('pets');
             $table->string('lat');
             $table->string('lon');
             $table->string('description')->nullable();
-            $table->foreignId('user_find_id')->nullable()->constrained('users');
+            $table->foreign('user_find_id')->references('id')->on('users');
             $table->datetime('lost_date');
             $table->boolean('status');
             $table->timestamps();
