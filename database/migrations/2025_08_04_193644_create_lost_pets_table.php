4c616608d4559b20a06928fa6fcce3f23c5e7208
fernandom11399 <jafetavalos49@gmail.com>
2025-08-04 20:20:14 -0600

    Update 2025_08_04_193644_create_lost_pets_table.php

--- database/migrations/2025_08_04_193644_create_lost_pets_table.php
+++ database/migrations/2025_08_04_193644_create_lost_pets_table.php
@@ -13,22 +13,25 @@ return new class extends Migration
     {
         Schema::create('lost_pets', function (Blueprint $table) {
             $table->id();
-
-            $table->unsignedBigInteger('user_id');
+                
+            $table->unsignedInteger('user_id');
             $table->unsignedBigInteger('pet_id');
-            $table->unsignedBigInteger('user_find_id')->nullable();
-
+            $table->unsignedInteger('user_find_id')->nullable();
+                
             $table->string('lat');
             $table->string('lon');
             $table->string('description')->nullable();
             $table->datetime('lost_date');
             $table->boolean('status');
+                
             $table->timestamps();
-
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreign('pet_id')->references('id')->on('pets')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->foreign('user_find_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
+                
+            // Foreign Keys
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('user_find_id')->references('id')->on('users')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreign('pet_id')->references('id')->on('pets')->cascadeOnDelete()->cascadeOnUpdate();
         });
+
     }
 
     /**
