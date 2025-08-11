af0de72206997bc8c5a6812c07687c7c33f068e1
Fernando <jafetavalos49@gmail.com>
2025-08-11 14:40:03 -0600

    Update CategoryProduct.php

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CategoryProduct extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $table = 'category_products';
 
