127def7053f7585735f9801bd58a4ec3142749f4
fernandom11399 <jafetavalos49@gmail.com>
2025-08-13 12:45:24 -0600

    Update AdopterController.php

--- app/Http/Controllers/api/Adoption/AdopterController.php
+++ app/Http/Controllers/api/Adoption/AdopterController.php
@@ -62,7 +62,7 @@ class AdopterController extends Controller
      */
     public function show(int $id)
     {
-        $adopter = Adopter::with('city', 'state', 'user')->find($id);
+        $adopter = Adopter::with('city', 'state', 'user', 'adoptions.pet')->find($id);
 
         if (!$adopter) {
             return response()->json([
